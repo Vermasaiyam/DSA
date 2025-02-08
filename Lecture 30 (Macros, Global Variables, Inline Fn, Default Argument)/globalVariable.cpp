b068358f32cd0951cf2cdee5ab91ea02a8ed120a
Vermasaiyam <vermasaiyam9@gmail.com>
2025-02-08 21:05:02 +0530

    global variables

---
+++ Lecture 30 (Macros, Global Variables, Inline Fn, Default Argument)/globalVariable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// using reference variable
+
+void a(int &i)
+{
+    cout << i << endl;
+    // b(i);
+}
+
+void b(int &i)
+{
+    cout << i << endl;
+}
+
+int main()
+{
+    int i = 5;
+    a(i);
+
+    return 0;
+}
