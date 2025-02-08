87ab2ea7f9c34030dc2a89d1575a8c598fe4211e
Vermasaiyam <vermasaiyam9@gmail.com>
2025-02-08 21:05:45 +0530

    inline fn

---
+++ Lecture 30 (Macros, Global Variables, Inline Fn, Default Argument)/inlineFunctions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+inline int getMax(int &a, int &b)
+{
+    return (a > b) ? a : b;
+}
+
+int main()
+{
+    int a = 1, b = 2;
+
+    int ans = getMax(a, b);
+
+    cout << ans;
+
+    a++;
+    b--;
+
+    ans = getMax(a, b);
+    cout << ans;
+
+    return 0;
+}
