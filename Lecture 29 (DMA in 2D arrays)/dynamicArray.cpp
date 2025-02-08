beb34965718210d45b006ceb4074397c03f7f3cd
Vermasaiyam <vermasaiyam9@gmail.com>
2025-02-08 21:03:28 +0530

    DMA in 2D arrays

--- Lecture 29 (DMA in 2D arrays)/dynamicArray.cpp
+++ Lecture 29 (DMA in 2D arrays)/dynamicArray.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int sum(int *arr, int n)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    return sum;
-}
-
 int main()
 {
-    int n;
-    cin >> n;
-
-    int *arr = new int[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+    int row, col;
+    cin >> row >> col;
+    int **arr = new int*[row];
+    
+    for (int i=0 ; i<row ; i++){
+        arr[i] = new int[col];
     }
-    cout << "Sum = " << sum(arr, n);
+    
+    // taking input
+    for (int i=0 ; i<row ; i++){
+        for (int j=0 ; j<col ; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    // taking output
+    for (int i=0 ; i<row ; i++){
+        for (int j=0 ; j<col ; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl; 
+    }
+    
+    // releasing memory
+    for (int i=0 ; i<row ; i++){
+        delete [] arr[i];
+    }
+    delete []arr;
 
     return 0;
 }
