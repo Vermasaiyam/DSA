22fbd56a8011887357942a62b358e27594a7abf1
Vermasaiyam <vermasaiyam9@gmail.com>
2025-05-12 00:07:09 +0530

    remove word in trie partial done

--- Lecture 79  (Trie & its implementation)/trieIntro.cpp
+++ Lecture 79  (Trie & its implementation)/trieIntro.cpp
@@ -78,3 +78,57 @@ int main(){
     cout << "Search TRIM: " << t->searchWord("TRIM") << endl;
     cout << "Search TRI: " << t->searchWord("TRI") << endl;
 }
+
+/*
+ //Return True if the Trie node has no child, else false
+    bool isEmpty(TrieNode* root)
+    {
+        for(int i=0;i<26;i++)
+            if(root->children[i])
+                return false;
+        
+        return true;
+    }
+
+    TrieNode* deleteUtil(TrieNode* root, string word)
+    {
+        if(root == NULL)
+            return NULL;
+        
+        //Last character being processed
+        if(word.length() == 0)
+        {
+            //This will not be a word after removal
+            if(root->isTerminal)
+                root->isTerminal = false;
+            
+            //If this is not a prefix of any other word. [not helping to make any other word]
+            if(isEmpty(root))
+            {
+                delete root;
+                root = NULL;
+            }
+            
+            return root;
+        }
+        
+        //Recursion call
+        int index = word[0] - 'a';
+        root->children[index] = deleteUtil(root->children[index], word.substr(1));
+        
+        //Backtracking
+        //If root has no child (as it's only child got deleted) and it is also not end of any other word
+        if(isEmpty(root) && root->isTerminal == false)
+        {
+            delete root;
+            root = NULL;
+        }
+        
+        return root;
+    }
+    
+    TrieNode* deleteWord(string word)
+    {
+        return deleteUtil(root, word);
+    }
+*/
